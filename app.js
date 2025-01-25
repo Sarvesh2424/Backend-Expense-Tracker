addc742158e9ca7fa70fbc3988352b20875dc542
Sarvesh2424 <10650sarvesh@gmail.com>
2025-01-25 14:29:36 +0530

    cors

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const { v4: uuidv4 } = require("uuid");
+const cors = require("cors");
 
 const app = express();
-app.use(express.json());
+app.use(cors());
 
 mongoose
   .connect(
